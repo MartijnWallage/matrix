fea590fe2e7f88a0f1136512d20920f0c07bbf37
Martijn Wallage <martijnwallage@gmail.com>
2026-09-13 12:17:15 +0100

    Passed first test

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,50 @@
 pub mod ex00;
 
-struct Vector::<K> {
+#[derive(Debug, Clone)]
+pub struct Vector<K> {
     data: Vec<K>,
 }
 
-struct Matrix::<K> {
+impl<K> Vector<K> {
+    pub fn new() -> Self {
+        Self { data: Vec::new() }
+    }
+
+    pub fn len(&self) -> usize {
+        self.data.len()
+    }
+
+    pub fn get(&self, i: usize) -> &K {
+        &self.data[i]
+    }
+
+    pub fn push(&mut self, new_data: K) {
+        self.data.push(new_data);
+    }
+}
+
+impl<K> From<Vec<K>> for Vector<K> {
+    fn from(data: Vec<K>) -> Self {
+        Self { data }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Matrix<K> {
     data: Vec<K>,
     rows: usize,
     cols: usize,
 }
 
-impl Matrix {
-    fn new(rows: usize, cols: usize) -> Self {
-        Matrix {Vec::new(), rows, cols}
+impl<K> Matrix<K> {
+    pub fn new(rows: usize, cols: usize) -> Self {
+        Matrix {data: Vec::new(), rows, cols}
     }
 
-    fn index(&self, x: u16, y: u16) -> u16 {
-        x + y * self.rows
-    } 
-}
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+    pub fn get(&self, x: usize, y: usize) -> &K {
+        let index = x * self.cols + y;
+        &self.data[index]
+    }
 }
 
 #[cfg(test)]
@@ -30,7 +53,13 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let mut v = Vector::<f64>::new();
+
+        assert_eq!(v.len(), 0);
+        v.push(1.0);
+        v.push(2.0);
+        v.push(3.0);
+        assert_eq!(v.len(), 3);
+        assert_eq!(*v.get(1), 2.0);
     }
 }
