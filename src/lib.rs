1eba50532ccdf049baf8a37251495be55a3f00ef
Martijn Wallage <martijnwallage@gmail.com>
2026-09-13 11:26:06 +0100

    Beginning

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+pub mod ex00;
+
+struct Vector::<K> {
+    data: Vec<K>,
+}
+
+struct Matrix::<K> {
+    data: Vec<K>,
+    rows: usize,
+    cols: usize,
+}
+
+impl Matrix {
+    fn new(rows: usize, cols: usize) -> Self {
+        Matrix {Vec::new(), rows, cols}
+    }
+
+    fn index(&self, x: u16, y: u16) -> u16 {
+        x + y * self.rows
+    } 
+}
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
