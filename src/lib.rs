197218541a878480e3b81824aad81b0c768a878b
Martijn Wallage <martijnwallage@gmail.com>
2026-09-15 18:14:19 +0100

    Complete vector and matrix structs

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,10 @@ impl<K> Vector<K> {
         self.data.len()
     }
 
+    pub fn is_empty(&self) -> bool {
+        self.data.is_empty()
+    }
+
     pub fn get(&self, i: usize) -> &K {
         &self.data[i]
     }
@@ -29,21 +33,38 @@ impl<K> From<Vec<K>> for Vector<K> {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Matrix<K> {
     data: Vec<K>,
     rows: usize,
     cols: usize,
 }
 
-impl<K> Matrix<K> {
-    pub fn new(rows: usize, cols: usize) -> Self {
-        Matrix {data: Vec::new(), rows, cols}
+impl<K: Clone> Matrix<K> {
+    pub fn new(rows: usize, cols: usize, data: K) -> Self {
+        Self {
+            data: vec![data; rows * cols],
+            rows,
+            cols,
+        }
     }
 
-    pub fn get(&self, x: usize, y: usize) -> &K {
-        let index = x * self.cols + y;
+    pub fn rows(&self) -> usize {
+        self.rows
+    }
+
+    pub fn cols(&self) -> usize {
+        self.cols
+    }
+
+    pub fn get(&self, col: usize, row: usize) -> &K {
+        let index = row * self.cols + col;
         &self.data[index]
+    }
+
+    pub fn set(&mut self, col: usize, row: usize, value: K) {
+        let index = row * self.cols + col;
+        self.data[index] = value;
     }
 }
 
@@ -52,14 +73,52 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn vector() {
         let mut v = Vector::<f64>::new();
 
         assert_eq!(v.len(), 0);
+        assert!(v.is_empty());
+
         v.push(1.0);
         v.push(2.0);
         v.push(3.0);
         assert_eq!(v.len(), 3);
+        assert_eq!(*v.get(0), 1.0);
         assert_eq!(*v.get(1), 2.0);
+        assert_eq!(*v.get(2), 3.0);
+
+        let vec = vec![0.1,0.2,0.3];
+        let v = Vector::<f64>::from(vec);
+        assert_eq!(v.len(), 3);
+        assert_eq!(*v.get(0), 0.1);
+        assert_eq!(*v.get(1), 0.2);
+        assert_eq!(*v.get(2), 0.3);
+    }
+
+    #[test]
+    fn matrix() {
+        let mut m = Matrix::<f64>::new(3, 3, 0.0);
+
+        assert_eq!(m.rows, 3);
+        assert_eq!(m.cols, 3);
+
+        assert_eq!(*m.get(0,0), 0.0);
+        assert_eq!(*m.get(1,1), 0.0);
+        assert_eq!(*m.get(2,2), 0.0);
+
+        m.set(0, 0, 1.0);
+        m.set(2, 1, 2.0);
+        m.set(1, 2, 3.0);
+
+        assert_eq!(
+            m,
+            Matrix::<f64> {
+                data: vec![1.0, 0.0, 0.0,
+                     0.0, 0.0, 2.0,
+                     0.0, 3.0, 0.0,
+                ],
+                rows: 3,
+                cols: 3,
+            });
     }
 }
