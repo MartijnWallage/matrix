131e32579164616d3d5a6a22170f8cb44f965931
Martijn Wallage <martijnwallage@gmail.com>
2026-09-22 10:45:26 +0100

    Fix logic error

--- src/ex01.rs
+++ src/ex01.rs
@@ -1,20 +1,24 @@
 use crate::Vector;
-use crate::Matrix;
 
 pub fn linear_combination(u: &[Vector<f64>], coefs: &[f64]) -> Vector<f64> {
-    if u.len() <= 0 {
+    if u.is_empty() {
         return Vector::<f64>::new();
     }
 
-    let mut v = Vector::<f64>::from(vec![0.0f64; u[0].len()]);
+    let mut result = Vector::<f64>::from(vec![0.0f64; u[0].len()]);
 
-    for i in 0..u.len() {
-        let mut uv = u[i].clone();
-        uv.scl(coefs[i]);
-        v.add(&uv);
+    // for each place in the vectors (the first vector)
+    // multiply by corresponding coefficient and add together
+    for i in 0..u[0].len() {
+        let mut acc = 0.0f64;
+        for j in 0..u.len() {
+            acc = coefs[j].mul_add(*u[j].get(i), acc);
+            println!("Acc: {}", acc);
+        }
+        result.set(i, acc);
     }
 
-    v
+    result
 }
 
 #[cfg(test)]
@@ -30,6 +34,7 @@ mod tests {
         let v1 = Vector::<f64>::from(vec![1.0f64, 2.0f64, 3.0f64]);
         let v2 = Vector::<f64>::from(vec![0.0f64, 10.0f64, -100.0f64]);
         
-        println!("Linear Combination: {}", linear_combination(&[e1, e2, e3], &[10.0f64, -2.0f64, 0.5f64]));
+        println!("Linear Combination e: {}", linear_combination(&[e1, e2, e3], &[10.0f64, -2.0f64, 0.5f64]));
+        println!("Linear Combination v: {}", linear_combination(&[v1, v2], &[10.0f64, -2.0f64]));
     }
 }
